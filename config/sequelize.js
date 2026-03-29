6497708e8894723e964550eb073c14c071cfba8e
HIRAPARANAYAN <47566420+HIRAPARANAYAN@users.noreply.github.com>
2026-03-29 13:56:06 +0530

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,17 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+dialect: "postgres",
+protocol: "postgres",
+logging: false,
+dialectOptions:
+process.env.NODE_ENV === "production"
+? {
+ssl: {
+require: true,
+rejectUnauthorized: false,
+},
+}
+: {},
+});
 
+export { sequelize };
 export default sequelize;
-
